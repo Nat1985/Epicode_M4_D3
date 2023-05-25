7cac943f21639c75aab129a258aa2d2e24c29498
Nat1985 <andreanatale85@gmail.com>
2023-05-25 18:32:47 +0200

    25 maggio

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 const apiUrl = "https://api.pexels.com/v1"; //l'indirizzo dell'endpoint da passare al fetch come url
 const apiKey = "tF2lGCLg3V2Sl90gHkjdJ6tastROqqRLls6CcktufE4ILNezlPgJLqY4"; //la api key da inserire in authorization nel header della chiamata fetch
+let mainDiv = document.getElementById("main");
 
-const getAlbum = () => {
-    fetch(apiUrl + "/search?query=SEA", { 
-        //fetch accetta due parametri: un url e un oggetto {qui ci sono diversi paramentri} headers: è un altro oggetto -> headers{authorization: API}
-    headers: {
-        authorization: apiKey
-        }
-    })
+function getAlbum() {
+    let userInput = document.getElementById("input-text").value;
+    console.log(userInput);
+    fetch(apiUrl + "/search?query=" + userInput, { 
+        headers: {
+            authorization: apiKey
+            }
+        })
     .then((res) => {
         return res.json()
     })
     .then((resJson) => {
-        console.log(resJson.photos)
+        putAlbum(resJson.photos)
     })
     .catch((err) => {console.log("Errore: " + err)});
 }
 
-getAlbum();
+function putAlbum(json) {
+    let img;
+    let bootCardDiv;
+    let bootCardBodyDiv;
+    let bootPCardBoyElement;
+    json.forEach(element => {
+        console.log(element);
+        img = document.createElement("img");
+        bootCardDiv = document.createElement("div");
+        bootCardBodyDiv = document.createElement("div");
+        bootPCardBoyElement = document.createElement("p");
+
+        bootCardDiv.classList.add("card");
+        bootCardDiv.style = "width: 18rem;";
+        img.src = element.src.tiny;
+        img.classList.add("card-img-top");
+        bootCardBodyDiv.classList.add("card-body");
+        bootPCardBoyElement.classList.add("card-text");
+        bootCardBodyDiv.textContent = element.alt;
+        mainDiv.appendChild(bootCardDiv);
+        bootCardDiv.appendChild(img);
+        bootCardDiv.appendChild(bootCardBodyDiv);
+        bootCardBodyDiv.appendChild(bootPCardBoyElement);
+    });
+}
